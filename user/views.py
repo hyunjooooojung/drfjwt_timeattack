1fc22622e6d73a6b4a1ab4caf97cd2cabb792b30
hyunjooooojung <hyunjoo3107@gmail.com>
2022-11-10 11:22:14 +0900

    make user app and start project

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+from user.models import User
+from user.serializers import CustomTokenObtainPairSerializer, UserSerializer
+
+
 
 # Create your views here.
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"회원 가입이 완료되었습니다!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+    
